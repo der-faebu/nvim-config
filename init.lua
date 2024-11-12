c9f0cbf37e167584f5080d08a3742abb6f839322
gfa@root.ch <gfa@root.ch>
2024-11-12 12:56:54 +0100

    added render-markdown

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
-require("settings")
+
 require("load-plugins")
 require("keymappings")
+require("settings")
 require("plugins")
-
--- lua/load-plugins.lua
+++ lua/load-plugins.lua
@@ -13,8 +13,21 @@ return require('packer').startup( function(use)
   use "tpope/vim-commentary"
   use "tpope/vim-repeat"
   use "tpope/vim-surround"
+  use 'iamcco/markdown-preview.nvim'
 
-use {
+
+use({
+    'MeanderingProgrammer/render-markdown.nvim',
+    after = { 'nvim-treesitter' },
+    requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+    equires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+        require('render-markdown').setup({})
+
+    end,
+})
+  use {
     's1n7ax/nvim-window-picker',
     tag = 'v2.*',
     config = function()
@@ -26,15 +39,19 @@ use {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
-  use {
+
+
+use {
     'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' 
   }
-     use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
-  end )
 
+use {
+    'nvim-treesitter/nvim-treesitter',
+     run = function()
+     local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+     ts_update()
+     end,
+    }
+
+
+  end )
