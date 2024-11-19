c6ae6decfb31b9d6713b24bc5117868ff6710b0b
gfa@root.ch <gfa@root.ch>
2024-11-19 09:02:54 +0100

    lsp first steps

--- lua/load-plugins.lua
+++ lua/load-plugins.lua
@@ -14,31 +14,61 @@ return require('packer').startup( function(use)
   use "tpope/vim-repeat"
   use "tpope/vim-surround"
   use 'iamcco/markdown-preview.nvim'
+  use 'mfussenegger/nvim-lint'
+
+-- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+--Autocompletion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
+
+-- Snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'
+
+-- -- LSP
+-- use{
+
+-- 'VonHeikemen/lsp-zero.nvim',
+-- requires = {
+-- -- LSP Support
+-- {'neovim/nvim-lspconfig'};
+-- }
+
+-- }
 
 
-use({
-    'MeanderingProgrammer/render-markdown.nvim',
-    after = { 'nvim-treesitter' },
-    requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
-    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
-    equires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
-    config = function()
-        require('render-markdown').setup({})
+  use({
+      'MeanderingProgrammer/render-markdown.nvim',
+      after = { 'nvim-treesitter' },
+      requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+      -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+      equires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+      config = function()
+          require('render-markdown').setup({})
 
-    end,
-})
+      end,
+  })
+
   use {
     's1n7ax/nvim-window-picker',
     tag = 'v2.*',
     config = function()
         require'window-picker'.setup()
     end,
-}
+  }
 
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
 
 use {
